cf994d7fcf801d44969a8b65268670b8f9c9cf2e
Dheerendra Singh <dhiru.all4u@gmail.com>
2024-03-15 12:01:40 +0530

    -

---
+++ WMI.Net/Model/Win32_Process.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WMI.Net.Model
+{
+    internal class Win32_Process
+    {
+        string CreationClassName;
+        string Caption;
+        string CommandLine;
+        DateTime CreationDate;
+        string CSCreationClassName;
+        string CSName;
+        string Description;
+        string ExecutablePath;
+        ushort ExecutionState;
+        string Handle;
+        uint HandleCount;
+        DateTime InstallDate;
+        ulong KernelModeTime;
+        uint MaximumWorkingSetSize;
+        uint MinimumWorkingSetSize;
+        string Name;
+        string OSCreationClassName;
+        string OSName;
+        ulong OtherOperationCount;
+        ulong OtherTransferCount;
+        uint PageFaults;
+        uint PageFileUsage;
+        uint ParentProcessId;
+        uint PeakPageFileUsage;
+        ulong PeakVirtualSize;
+        uint PeakWorkingSetSize;
+        uint Priority;
+        ulong PrivatePageCount;
+        uint ProcessId;
+        uint QuotaNonPagedPoolUsage;
+        uint QuotaPagedPoolUsage;
+        uint QuotaPeakNonPagedPoolUsage;
+        uint QuotaPeakPagedPoolUsage;
+        ulong ReadOperationCount;
+        ulong ReadTransferCount;
+        uint SessionId;
+        string Status;
+        DateTime TerminationDate;
+        uint ThreadCount;
+        ulong UserModeTime;
+        ulong VirtualSize;
+        string WindowsVersion;
+        ulong WorkingSetSize;
+        ulong WriteOperationCount;
+        ulong WriteTransferCount;
+    }
+}
